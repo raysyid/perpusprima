32901103cb2cd2ee6d7f79520dc4e91dd9651f33
SayyidIlmi <sayyidilmihubballillah@gmail.com>
2025-11-13 12:38:44 +0700

    session

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\BooksController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,9 +13,17 @@ Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
 });
 
-// CRUD
+// CRUD lama
 Route::post('/students', [StudentController::class, 'store']);
 Route::get('/students', [StudentController::class, 'index']);
+Route::delete('/students/{nim}', [StudentController::class, 'destroy']);
+Route::put('/students/{nim}', [StudentController::class, 'update']);
+Route::patch('/students/{nim}', [StudentController::class, 'update']);
+
+// CRUD baru
+
+Route::post('/books', [StudentController::class, 'store']);
+Route::get('/books', [StudentController::class, 'index']);
 Route::delete('/students/{nim}', [StudentController::class, 'destroy']);
 Route::put('/students/{nim}', [StudentController::class, 'update']);
 Route::patch('/students/{nim}', [StudentController::class, 'update']);
@@ -25,4 +34,6 @@ Route::post('/login', [AuthController::class,'login']);
 Route::middleware(['dummy.jwt'])->group(function() {
 Route::post('/logout', [AuthController::class,'logout']);
 Route::get('/profile', [AuthController::class,'profile']);
+
+
 });
